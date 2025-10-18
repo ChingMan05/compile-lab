6ef368d0c7dcb6402823f9223e97e3706e669ba1
ChingMan05 <859766083@qq.com>
2025-10-18 17:59:02 +0800

    修改测试文件

--- lab2_all/lab2/test1-23.sh
+++ lab2_all/lab2/test1-23.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 
-TEST_DIR="../Test"
-PARSER="./parser"
+TEST_DIR="../test1-23"
 
-echo "开始测试 test1.cmm ~ test23.cmm ..."
+PARSER="./parser" 
+
+echo "开始测试所有.cmm文件..."
 echo ""
 
 count=0
-for i in $(seq 1 23); do
-    cmm_file="$TEST_DIR/test${i}.cmm"
+for cmm_file in "$TEST_DIR"/*.cmm; do
     if [ -f "$cmm_file" ]; then
         count=$((count + 1))
-        echo "========== 测试 $count: test${i}.cmm =========="
+        echo "========== 测试 $count: $(basename "$cmm_file") =========="
         $PARSER "$cmm_file"
-        echo ""
-    else
-        echo "跳过：未找到文件 test${i}.cmm"
         echo ""
     fi
 done
